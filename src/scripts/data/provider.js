c774c9e0d5dba3bfbc423abee8d6da228621217f
DougSilvera <shortxjdoug@gmail.com>
2022-01-05 10:44:25 -0500

    added database objects and fetch/get functions

--- src/scripts/data/provider.js
+++ src/scripts/data/provider.js
@@ -1,12 +1,58 @@
-const apiURL = "http://localhost:8088"
-const applicationElement = document.querySelector(".giffygram")
-
+const apiURL = "http://localhost:8088";
+const applicationElement = document.querySelector(".giffygram");
 
 const applicationState = {
-    currentUser: {},
-    feed: {
-        chosenUser: null,
-        displayFavorites: false,
-        displayMessages: false
-    }
-}
+  users: [],
+  posts: [],
+  likes: [],
+  messages: [],
+  currentUser: {},
+  feed: {
+    chosenUser: null,
+    displayFavorites: false,
+    displayMessages: false,
+  },
+};
+
+export const fetchUsers = () => {
+  return fetch(`${apiURL}/users`)
+    .then((response) => response.json())
+    .then((assignedUser) => {
+      applicationState.users = assignedUser;
+    });
+};
+
+export const fetchPosts = () => {
+  return fetch(`${apiURL}/posts`)
+    .then((response) => response.json())
+    .then((assignedPost) => {
+      applicationState.posts = assignedPost;
+    });
+};
+export const fetchLikes = () => {
+  return fetch(`${apiURL}/likes`)
+    .then((response) => response.json())
+    .then((assignedLike) => {
+      applicationState.likes = assignedLike;
+    });
+};
+export const fetchMessages = () => {
+  return fetch(`${apiURL}/messages`)
+    .then((response) => response.json())
+    .then((assignedMessage) => {
+      applicationState.messages = assignedMessage;
+    });
+};
+
+export const getUsers = () => {
+  return applicationState.users.map((user) => ({ ...user }));
+};
+export const getPosts = () => {
+  return applicationState.posts.map((post) => ({ ...post }));
+};
+export const getLikes = () => {
+  return applicationState.likes.map((like) => ({ ...like }));
+};
+export const getMessages = () => {
+  return applicationState.messages.map((message) => ({ ...message }));
+};
